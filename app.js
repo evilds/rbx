387d6ac13823e41a3dc19ae9393b5f3c6746006c
evil <70036667+evilds@users.noreply.github.com>
2022-08-22 15:48:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,33 @@ app.post("/ProxyGet", function(Request, Res){
     request(URL).pipe(Res)
 });
 
+app.post("/Webhook", function(Request, Res){
+  if (!Request.body.content){
+      Res.send("No content provided")
+      return
+  }
+  if (!Request.body.url){
+      Res.send("No url provided")
+      return
+  }
+
+  var URL = Request.body.url
+  var CONTENT = Request.body.content
+
+  console.log(URL)
+  console.log(CONTENT)
+
+  var j = {
+    "content": CONTENT
+  }
+
+  request.post({
+    "headers": {'content-type' : 'application/json'},
+    "url": URL,
+    "body": JSON.stringify(j)
+  });
+});
+
 app.listen(port, () => {
   console.log("Server Loaded.");
 });
